55de4527a9812ac44410378818e7d5a3decabb54
Your Name <you@example.com>
2021-02-22 17:29:44 +0630

    add: include add:child_category_blade

---
+++ resources/views/category/child_category.blade.php
@@ -0,0 +1,9 @@
+<li>{{$child_category->name}}</li>
+@if ($child_category->categories)
+    <ul>
+        @foreach ($child_category->categories as $childCategory)
+            @include('category.child_category',['child_category'=>$childCategory])
+        @endforeach
+    </ul>
+@endif
+
--- resources/views/category/view.blade.php
+++ resources/views/category/view.blade.php
@@ -8,21 +8,7 @@
                 <li>{{$category->name}}</li>
                 <ul>
                     @foreach ($category->categories as $childCategory)
-                       <li>{{$childCategory->name}}</li>
-                       @if ($childCategory)
-                           <ul>
-                               @foreach ($childCategory->categories as $child2Category)
-                                   <li>{{$child2Category->name}}</li>
-                                   @if ($child2Category)
-                                       <ul>
-                                           @foreach ($child2Category->categories as $child3Category)
-                                               <li>{{$child3Category->name}}</li>
-                                           @endforeach
-                                       </ul>
-                                   @endif
-                               @endforeach
-                           </ul>
-                       @endif
+                      @include('category.child_category',['child_category'=>$childCategory])
                     @endforeach
                 </ul>
             @endforeach
